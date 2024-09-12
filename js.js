7e9f489ed54c4771e61fb4180fed8dfc2439c74a
Tomi0928 <tomi09282@gmail.com>
2024-09-12 09:41:44 +0200

    Util function factorial

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
 function getFirstElement (arr){
     //ide írd kódod
     }
+    function factorial (n) {
+      if (n == 0 || n == 1)
+        return 1;
+      if (f[n] > 0)
+        return f[n];
+      return f[n] = factorial(n-1) * n;
+    }
     const numbers = [1, 2, 3];
     const words = [apple, banana, cherry];
     console.log(`First number: ${getFirstElement(numbers)}`);
